55675f2a398c55961f969df3e14bdd55c2a1da0a
Romeo Disca <romeo.disca@gmail.com>
2022-12-28 19:34:06 +0100

    finished player

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::process::{Child, Command};
+use std::path::{Path, PathBuf};
+use std::process::Command;
 use std::sync::Arc;
 use std::time::Duration;
+use glob::Paths;
 
 use tokio::spawn;
 use tokio::sync::RwLock;
@@ -8,26 +10,32 @@ use tokio::task::JoinSet;
 use tokio::time::sleep;
 use watchman_client::prelude::*;
 use watchman_client::SubscriptionData;
+use rand::Rng;
+
 
 #[derive(Default)]
 struct App {
     state: Arc<RwLock<State>>,
 }
 
-#[derive(Default)]
 pub struct State {
-    file: Option<String>,
-    command: Option<String>,
-    cmd: Option<Command>,
-    child: Option<Child>,
+    parameter: Vec<String>,
     do_play: bool,
+}
+impl Default for State {
+    fn default() -> Self {
+        State {
+            parameter: vec![format!("{}/{}", sound_dir(), default_sound())],
+            do_play: false,
+        }
+    }
 }
 impl App {
     pub async fn watch(&self) -> anyhow::Result<()> {
         let state = self.state.clone();
-        let mut client = Connector::new().connect().await?;
+        let client = Connector::new().connect().await?;
         let resolved = client
-            .resolve_root(CanonicalPath::canonicalize("./sounds")?)
+            .resolve_root(CanonicalPath::canonicalize(sound_dir())?)
             .await?;
         let (mut subscription, _) = client
             .subscribe(
@@ -40,8 +48,21 @@ impl App {
                 let next: SubscriptionData<NameOnly> = subscription.next().await?;
 
                 match next {
-                    SubscriptionData::FilesChanged(result) => {
+                    SubscriptionData::FilesChanged(_result) => {
                         state.write().await.do_play = true;
+                        let mut v = vec![];
+                        for entry in glob::glob(&format!("{}/*", sound_dir())).unwrap() {
+                            match entry {
+                                Ok(path) => {
+                                    v.push(path.display().to_string());
+                                }
+                                _ => {}
+                            }
+                        }
+                        let num = rand::thread_rng().gen_range(0..v.len() + 10000);
+                        if let Some(entry) = v.get(num % v.len()).cloned() {
+                            state.write().await.parameter = vec![entry];
+                        }
                     }
                     _ => {}
                 }
@@ -56,8 +77,8 @@ impl App {
         let jh = spawn(async move { loop {
                 let play = state.read().await.do_play;
                 if play {
-                    let mut command = Command::new("play");
-                    command.args(["./sounds/total_commitment.wav"]);
+                    let mut command = command(&state.read().await.parameter[..]);
+                    log::info!("{:?}", command);
                     if let Ok(mut child) = command.spawn() {
                         match child.wait().map_err(anyhow::Error::from) {
                             Ok(_) => {}
@@ -81,7 +102,7 @@ impl App {
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
-    
+
     let app = Arc::new(App::default());
     let mut set = JoinSet::new();
     let _app = app.clone();
@@ -90,4 +111,37 @@ async fn main() -> anyhow::Result<()> {
     set.spawn(async move { _app.watch().await?; anyhow::Ok(()) });
     while let Some(Ok(_)) = set.join_next().await {}
     Ok(())
+}
+
+pub fn command(subsitutes: &[String]) -> Command {
+    let mut command = Command::new("echo");
+    command.arg("nothing to do.");
+    let mut sub = 0;
+    if let Ok(value) = dotenv::var("cmd") {
+        for (i, part) in value.split(" ").enumerate() {
+            let mut part = part.trim();
+            log::info!("{part}");
+            if part.eq("") {
+                continue;
+            }
+            let s;
+            if part.eq("{}") {
+                s = part.replace("{}", subsitutes.get(sub).unwrap_or(&"{}".to_string()));
+                part = &s;
+                sub += 1;
+            }
+            match i {
+                0 => command = Command::new(part),
+                _ => { command.arg(part); },
+            }
+        }
+    }
+    command
+}
+
+pub fn sound_dir() -> String {
+    dotenv::var("sound_dir").unwrap_or("./sounds".to_string())
+}
+pub fn default_sound() -> String {
+    dotenv::var("default_sound").unwrap_or("".to_string())
 }
